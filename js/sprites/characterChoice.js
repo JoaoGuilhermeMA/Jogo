aeb4a9f47e0fb93237b6915b07bbcf87fe6a0188
JoaoGuilhermeMA <joomoreira36@gmail.com>
2023-11-16 04:33:13 -0300

    digita nome e escolhe personagem

--- js/sprites/characterChoice.js
+++ js/sprites/characterChoice.js
@@ -43,3 +43,50 @@ function startAnimations() {
 }
 
 startAnimations();
+
+// Obtendo todos os elementos article
+const articles = document.querySelectorAll('.articles article');
+let currentPlayer = 1; // Controla o jogador atual
+
+// Função para atualizar o título com o nome do jogador
+const updateTitle = () => {
+  const playerName = localStorage.getItem(`player${currentPlayer}Name`);
+  if (playerName) {
+    document.querySelector('h1').textContent = `Escolha de ${playerName}`;
+  } else {
+    document.querySelector('h1').textContent = `Escolha do jogador ${currentPlayer}`;
+  }
+};
+
+// Chamando a função para definir o título inicial
+updateTitle();
+
+// Adicionando um evento de clique a cada elemento article
+articles.forEach((article, index) => {
+  article.addEventListener('click', () => {
+    const player1Selection = localStorage.getItem('player1Character');
+    const player2Selection = localStorage.getItem('player2Character');
+
+    if (currentPlayer === 1 && !player1Selection) {
+      localStorage.setItem('player1Character', index + 1);
+      console.log(`Jogador 1 selecionou o personagem ${index + 1}.`);
+      currentPlayer = 2; // Muda para o próximo jogador
+    } else if (currentPlayer === 2 && !player2Selection) {
+      localStorage.setItem('player2Character', index + 1);
+      console.log(`Jogador 2 selecionou o personagem ${index + 1}.`);
+      currentPlayer = 1; // Muda para o próximo jogador
+
+      // Verifica se ambos os jogadores já fizeram suas seleções
+      if (localStorage.getItem('player1Character') && localStorage.getItem('player2Character')) {
+        console.log('Ambos os jogadores fizeram suas seleções.');
+        // Redireciona para a página jogar.html após a seleção do segundo jogador
+        window.location.href = 'jogar.html';
+      }
+    }
+
+    // Atualiza o título para exibir o próximo jogador
+    updateTitle();
+  });
+});
+
+
